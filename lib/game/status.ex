4300eaa926287fb022d90532c7bca96bef5d9bc5
Gabriel Henrique <gabriel.henrique@agenciam3.com>
2024-03-14 23:24:23 -0300

    feat: ExMon game finished

---
+++ lib/game/status.ex
@@ -0,0 +1,34 @@
+defmodule ExMon.Game.Status do
+  def print_round_message(%{status: :started} = info) do
+    IO.puts("\n==== The game is started! ====\n")
+    IO.inspect(info)
+    IO.puts("-----------------")
+  end
+
+  def print_round_message(%{status: :continue, turn: player} = info) do
+    IO.puts("\n==== It's #{player} turn ====\n")
+    IO.inspect(info)
+    IO.puts("-----------------")
+  end
+
+  def print_round_message(%{status: :game_over} = info) do
+    IO.puts("\n==== The game is over ====\n")
+    IO.inspect(info)
+    IO.puts("-----------------")
+  end
+
+  def print_valid_move_message(move), do: IO.puts("\n===== Valid move: #{move}. =====\n")
+  def print_wrong_move_message(move), do: IO.puts("\n===== Invalid move: #{move}. =====\n")
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\n===== The player attacked the computer dealing #{damage} damage. =====\n")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("\n===== The computer attacked the player dealing #{damage} damage. =====\n")
+  end
+
+  def print_move_message(player, :heal, life) do
+    IO.puts("\n===== The #{player} healled itself to #{life} life points. =====\n")
+  end
+end
